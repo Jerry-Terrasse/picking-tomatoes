dce0bc3268ed8be6308467f2ce76d32b1df1835b
Terrasse <terrasse@qq.com>
2023-08-05 17:39:19 +0800

    add draw_hist

--- stats.py
+++ stats.py
@@ -1,12 +1,24 @@
 import glob
 import json
 import sys
+from collections import Counter
+import matplotlib.pyplot as plt
 
 files = glob.glob(f'{sys.argv[1]}/*.json')
 print(f"{len(files)} files")
 cnt = 0
+results = []
 for fname in files:
     with open(fname) as f:
         data = json.load(f)
     cnt += len(data)
+    results.extend([r['rating'] for r in data.values()])
 print(f"{cnt} records")
+print(Counter(results))
+
+def draw_hist(results, path="hist.png"):
+    plt.hist(results, bins=10)
+    plt.savefig(path)
+    plt.show()
+
+draw_hist(results)
